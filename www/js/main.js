3a6b380252ecf44c520d4487b775723e2476d993
Bill Marrs <billmarrs@gmail.com>
2017-07-27 13:40:23 -0400

    V

--- www/js/main.js
+++ www/js/main.js
@@ -2,6 +2,9 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 var game = new Phaser.Game("100%", "100%", Phaser.AUTO);
 
+// place for persistent storage (like score), accessible from any state
+var V = {};
+
 game.state.add('Boot', SweetieGame.Boot);
 game.state.add('Preloader', SweetieGame.Preloader);
 game.state.add('Game', SweetieGame.Game);
