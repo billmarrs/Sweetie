8245379672415d9b0f33ae9d521c37d6242f2532
Bill Marrs <billmarrs@gmail.com>
2017-07-31 13:07:04 -0400

    hide money, dconsole sprite, click anywhere to go back to home

--- www/js/Pet.js
+++ www/js/Pet.js
@@ -1,6 +1,7 @@
 
 SweetieGame.Pet = function(game) {
     this.padding = 15;
+    this.dconsole = null;
     // NYI pull money from storage somehow
     if (typeof V.money === "undefined") V.money = 100;
     if (typeof V.mood === "undefined") V.mood = 'cranky'; // NYI random
@@ -14,15 +15,29 @@ SweetieGame.Pet.prototype = {
 
     create: function() {
 
+	if (typeof game.debugLevel !== "undefined" &&
+	    this.game.debugLevel === "onscreen") {
+	    this.dconsole = this.game.add.text(this.padding, this.padding, '',
+					       { font: "20px Arial", fill: "#666666", align: "left" });
+	    this.dconsole.setText(V.consoleLog.reverse().join("\n"));
+	}
+
 	this.nyi = this.add.bitmapText(this.game.width/2, this.game.height/2, 'Banner', 'NYI Pet', 48);
 	this.nyi.anchor.set(.5);
-	this.nyi.inputEnabled = true;
-	this.nyi.events.onInputDown.add(this.goHome, this);
-	
+
+	this.input.onDown.add(this.goHome, this);
+
+	if (typeof game.debugLevel !== "undefined" &&
+	    this.game.debugLevel === "onscreen") {
+	    this.dconsole = this.game.add.text(this.padding, this.padding, '',
+					       { font: "20px Arial", fill: "#666666", align: "left" });
+	    this.dconsole.setText(V.consoleLog.reverse().join("\n"));
+	}
+
 	// Banner text
-	this.moneyText = this.add.bitmapText(this.padding, this.padding, 'Banner', '$'+V.money, 48);
-	this.moneyText.tint = 0x00ff00;
-	this.moneyText.anchor.set(0,0);
+// 	this.moneyText = this.add.bitmapText(this.padding, this.padding, 'Banner', '$'+V.money, 48);
+// 	this.moneyText.tint = 0x00ff00;
+// 	this.moneyText.anchor.set(0,0);
 	this.moodText = this.add.bitmapText(this.game.width-this.padding, this.padding, 'Banner', V.mood, 48);
 	this.moodText.anchor.set(1,0);
 
@@ -31,11 +46,14 @@ SweetieGame.Pet.prototype = {
     },
     
     update: function() {
-
+	if (V.consoleUpdate) {
+	    this.dconsole.setText(V.consoleLog.reverse().join("\n"));
+	    V.consoleUpdate = false;
+	}
     },
 
     orientAll:function() {
-	this.warn('orientAll (w/h) = ('+this.game.width+'/'+this.game.height+')');
+	warn('orientAll (w/h) = ('+this.game.width+'/'+this.game.height+')');
 	this.moodText.x = this.game.width-this.padding;
     },
 	//this.game.stage.backgroundColor = "#444444";
