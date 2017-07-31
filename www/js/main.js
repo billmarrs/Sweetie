9b2af4bb1cbbeb77245b6b84bbb38429a3859727
Bill Marrs <billmarrs@gmail.com>
2017-07-31 13:06:08 -0400

    move warn, v.consoleLog here

--- www/js/main.js
+++ www/js/main.js
@@ -4,6 +4,7 @@ var game = new Phaser.Game("100%", "100%", Phaser.AUTO);
 
 // place for persistent storage (like score), accessible from any state
 var V = {};
+V.consoleLog = [];
 
 game.state.add('Boot', SweetieGame.Boot);
 game.state.add('Preloader', SweetieGame.Preloader);
@@ -15,4 +16,14 @@ function onDeviceReady() {
     game.state.start('Boot');
 }
 
-
+function warn(msg) {
+    var type = typeof game.debugLevel;
+    if (type === "undefined") return null;
+    if (game.debugLevel === "onscreen") {
+	V.consoleLog.unshift(msg);
+	V.consoleLog = V.consoleLog.slice(0,5); // limit length to 3 lines
+	V.consoleUpdate = true;
+    } else {
+	console.log(msg);
+    }
+}
