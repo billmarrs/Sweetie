31df5478ca086c43701f14d7e12d3805c23b3a7f
Bill Marrs <billmarrs@gmail.com>
2017-08-02 15:04:47 -0400

    mood methods

--- www/main.js
+++ www/main.js
@@ -2,10 +2,9 @@ var game = new Phaser.Game("100%", "100%", Phaser.AUTO);
 
 // Persistent storage/utility object
 var V = {
-    mood: 'cranky',
+    mood: 4,
 
     debugLevel: null, // null to disable, "onscreen" or anything else for console logging
-
     consoleLog: ['start'],
     consoleUpdate: false,
     
@@ -32,7 +31,61 @@ var V = {
     displayLogClearUpdate: function() {
 	this.consoleUpdate = false;
 	return this.consoleLog.slice().reverse().join("\n");
-    }
+    },
+
+    moodToString: function() {
+	switch(this.mood) {
+	case 0:
+	    return 'inconsolable';
+	    break;
+	case 1:
+	    return 'despondent';
+	    break;
+	case 2:
+	    return 'irritated';
+	    break;
+	case 3:
+	    return 'cranky';
+	    break;
+	case 4:
+	    return 'content';
+	    break;
+	case 5:
+	    return 'asleep';
+	    break;
+	default:
+	    return 'indeterminate';
+	}	    
+    },
+
+    lowerMood: function() {
+	if (this.mood > 0) this.mood--;
+    },
+    
+    moodToTint: function() {
+	switch(this.mood) {
+	case 0:
+	    return 0x990000;
+	    break;
+	case 1:
+	    return 0xff3333;
+	    break;
+	case 2:
+	    return 0xff6666;
+	    break;
+	case 3:
+	    return 0xff9999;
+	    break;
+	case 4:
+	    return 0xffCCCC;
+	    break;
+	case 5:
+	    return 0xffffff;
+	    break;
+	default:
+	    return 0xffffff;
+	}
+    },
 }
 
 game.state.add('Boot', SweetieGame.Boot);
